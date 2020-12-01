6205c53a0923c67d16b5f3242424dff44a7f08d9
reneMX <rene.martinez.magana@estudiante.uacm.edu>
2020-12-01 06:40:43 -0600

    Version 0.0.1 Segunda Version Rene Martinez M Practica5 Memoria 1 de Diciembre 2020 06:00 am
    FUNCIONALIDADES
    En esta version se pude crear 1 o 4 procesos, dependiendo el operador ingresado
    Por el momento esta limitada a funcionar con :
    ---Suma +
    ---Resta -
    ---Multiplicacion x
    ---Division /
    ---Todas las operaciones #
    ................................
    Lo que realiza el programa:
    >Permite la entrada por consola de una expresion, junta sin espacio, con 2 operandos y 1 operador, formando una operacion, solo con los operadores anteriores
    >Creara 1 proceso si selecciono un operador distinto a #, es decir, si ingresamor 5+2, creara 1 solo proceso hijo, sin encambio, nosotros ingresamos el operador #(gato), podemos crear todos los demas procesos.
    >
    En base a lo anterior, el programa, una vez creado el o los procesos, sustituira por sus respectivos procesos, suma,resta..etc.
    ................................
    FALTANTES
    *COMUNICACION MEDIANTE MEMORIA COMPARTIDA ENTRE PADRE E HIJOSUSTITUTO(padre-suma o padre-resta o, etc.)
    CREACION DEL SCRIPT.H, PARA PODER CORRER EL PROGRAMA DESDE 1 SOLA INSTRUCCION EN TERMINAL.
    RESPONDER PREGUNTAS DEL CUEATIONARIO.
    ................................

--- calcu.c
+++ calcu.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 
 
-char obtenOperador(char *ope);
+
+char verificaOperador(char *ope);
 void obtenOperandos(char *ando, char *operandos);
-// char obtenOperando(char *ope);
+void duplicaSustituyeProceso(char operador);
+
+char *const  arvg2_suma[] = {"suma", "Sustituido", "Sumar", NULL} ;
+char *const  arvg2_resta[] = {"resta", "Sustituido", "Restar", NULL} ;
+char *const  arvg2_multi[] = {"Multi", "Sustituido", "Multiplicar", NULL} ;
+char *const  arvg2_div[] = {"Division", "Sustituido", "Dividir", NULL} ;
 
 
 int main(int argc, char *argv[]){
@@ -35,51 +43,149 @@ int main(int argc, char *argv[]){
 
         operandos = (char*) malloc(strlen(*argv) + 1);
 
-/*OBTENER OPERADOR Y OPERANDO*/
-        /*
+/*VALIDAR Y OBTENER OPERADOR*/
+
+        /* 
+            char verificaoperadOperador(char *);
+            
             *Con esta funcion obtenemos el operador
-            *El cual,se guarda en 
+            Return
+            *Esta funcion retorna  el operador, solo en caso de encontrarlo
+            *O en caso contrario, regresa la letra 'e', que significara Error, y operador invalido
         */
+        operador = verificaOperador(ope);
 
-        operador = obtenOperador(ope);
-        printf("Operador dentro del main %c \n", operador);
-
-        switch ( operador )
+        if( operador != 'e')//evaluamos lo que devuelve la funcion
         {
-            case '+':
-                // pid_s = fork();
-                printf("Operador %c \n", operador);
-            break; 
+            /*
+                void obtenOperando(char *);
+                *Con este metodo obtenemos, los operandos
+            */
+            obtenOperandos(ope, operandos);
+             
+             /*
+                void duplicaSustituyeProceso(char );
+                *Con este metodo duplicaremos el proceso las veces que el operador lo indique
+                *Ademas, realizaremos la sustitucion del proceso, 
+                    *justo cuando estemos en el operador seleccionado.
+            */
+            duplicaSustituyeProceso( operador );
 
-            case '-' :
-                // pid_s = fork();
-                printf("Operador %c \n", operador);
-            break; 
+            
 
-            case '*' :
-                // pid_s = fork();
-                printf("Operador %c \n", operador);
-            break; 
+            
+        }//fin if operador
 
-            case '/' :
-                // pid_s = fork();
-                printf("Operador %c \n", operador);
-            break; 
-
-            case '#' :
-                 // pid_s = fork();
-                printf("Operador %c \n", operador);
-            break; 
-        }  
-
-        obtenOperandos(ope, operandos);
-         
-
-         free(ope);
-         free(ando);
+            while(1);
+        free(ope);
+        free(ando);
     return 0; 
 }
 
+
+void duplicaSustituyeProceso(char operador)
+{
+    pid_t pid;
+    
+        switch ( operador )
+        {
+            //CASO +//
+            case '+':
+                pid = fork();
+                if( pid == 0 )
+                {
+                    //printf("Soy el proceso  hijo %d\n", getpid());                
+                    sleep(5);
+                    execv("./suma", arvg2_suma);
+                    printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                }
+                while(1);
+                //printf("Para clonar + \n");
+            break;
+
+            //CASO - //
+            case '-':
+                pid = fork();
+                if( pid == 0 )
+                {
+                    //printf("Soy el proceso  hijo %d\n", getpid());    
+                    sleep(5);
+                    execv("./resta", arvg2_resta);
+                    printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                }
+                while(1);
+                //printf("Para clonar - \n");
+            break;
+
+            //CASO x //
+            case 'x':
+                pid = fork();
+                if( pid == 0 )
+                {
+                    //printf("Soy el proceso  hijo %d\n", getpid());                  
+                    sleep(5);
+                    execv("./multi", arvg2_multi);
+                    printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                }
+                while(1);                
+                //printf("Para clonar x \n");
+            break;
+
+            //CASO / //
+            case '/':
+                pid = fork();
+                if( pid == 0 )
+                {
+                    //printf("Soy el proceso  hijo %d\n", getpid());    
+                    sleep(5);
+                    execv("./division", arvg2_div);
+                    printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                }
+                while(1);                
+                //printf("Para clonar / \n");
+            break;
+            
+            //CASO # //
+            case '#':
+                for ( int i = 0 ; i < 4 ; i++ )
+                {
+                    pid = fork();
+                    if( pid == 0)
+                    {
+                        sleep(5);
+                        switch (i)
+                        {
+                            //CASO DOND i = 0//
+                            case 0:                                                         
+                                execv("./suma", arvg2_suma);
+                                printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                            break ;
+
+                            //CASO DOND i = 1//
+                            case 1:
+                                execv("./resta", arvg2_resta);
+                                printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                            break ;
+
+                            //CASO DOND i = 2//
+                            case 2:
+                                execv("./multi", arvg2_multi);
+                                printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                            break ;
+
+                            //CASO DOND i = 3//
+                            case 3:
+                                execv("./division", arvg2_div);
+                                printf("Error\n");//Solo en caso de que no se haga el cambio de proceso con exec 
+                            break ;
+                        }
+                        break;
+                    }
+                }
+            break;        
+        }
+
+}//Fin metodo Duplica
 
 
 
@@ -87,54 +193,87 @@ void obtenOperandos(char *ando, char *operandos)
 {
     char aux ;
     const char delim[] = "+-*/#";
-    // const char delim1[] ="[0|1|2|3|4|5|6|7|8|9][.][0|1|2|3|4|5|6|7|8|9]";
     char *token = strtok(ando, delim);
-    
     int i = 0 ;
+
     while(token != NULL)
     {   
         aux = token[i];
-        strcat(operandos, " ");
+        strcat(operandos, " ");//agrego un espacio, para que no se junten los operandos
         strcat(operandos, token);
-        // printf("Dentro de la funcion token %s\n",token);
         token = strtok(NULL, delim);
         i++;
     }
-}//Fin obtenOperador
+}//Fin metodo obtenOperador
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-char obtenOperador(char *ope)
+char verificaOperador(char *ope)
 {
-    char aux ;
-    //  const char delim1[] = "+-*/#";
-    const char delim1[] ="[0|1|2|3|4|5|6|7|8|9][.][0|1|2|3|4|5|6|7|8|9]";
-    char *token = strtok(ope, delim1);
+    //Declaracion de variables
+    char aux = ' ' ;
+    char aux2 ;
     int i = 0 ;
-    while(token != NULL)
-    {   
-        aux = token[i];
-        printf("Dentro de la funcion token %s\n",token);
-        token = strtok(NULL, delim1);
-        i++;
-    }
-    return aux; 
+    //Iteramos en el apuntador donde esta nuestra cadena
+    while ( i < strlen(ope) )
+    {
+        aux = ope[i];
+        switch ( aux )
+        {
+            case '+':
+                aux2 = '+';
+                printf("operador + valido\n");
+            break;
+            case '-':
+                aux2 = '-';
+                printf("operador - valido \n");
+            break;
+            case 'x':
+                aux2 = 'x';
+                printf("operador x valido \n");
+            break;
+            case '/':
+                aux2 = '/';
+                printf("operador / valido \n");
+            break;
+            case '#':
+                aux2 = '#';
+                printf("operador # valido\n");
+            break;        
+        }
+
+       i++;
+    }//fin while
     
+    if( aux2 != '+' &&  aux2 != '-' && aux2 != 'x' && aux2 != '/' && aux2 != '#' )
+    { 
+        printf("Operador No incluido en esta calculadora\n"); 
+        aux2 = 'e';
+    }
+    
+    return aux2;
 }//Fin obtenOperador
+ 
+
+
+
+ /*
+ pid_t pid;
+            if( operador == '+' )
+            {
+                pid = fork();
+                    if( pid == 0 )
+                    {
+                        printf("Soy el proceso  hijo %d\n", getpid());    
+                        printf("%s : %d \n ",argv[0] , getpid());  
+                        
+                        char *const arvg2[] = {"suma", "sustituido", "Sumar", NULL} ;
+                        sleep(5);
+                        execv("./suma", arvg2);
+
+                        printf("Error\n"); 
+                        
+                    }
+            }//fin if
+ 
+ 
+ */
---
+++ division.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+    printf("Proceso Division\n");
+    printf("%s : %d \n ", argv[0], getpid());
+    for(int i = 0; i <  argc ; i ++)
+    {
+        printf("Argumento %d,  %s\n", i, argv[i]);
+    }
+    while(1);
+
+return 0;
+}//fin main
